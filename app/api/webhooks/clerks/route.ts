060a1789b80f4b53f78bbe16b80d25ac97e2ca5b
saumya-14 <saumya140904@gmail.com>
2025-01-05 00:30:19 +0530

    correcting

--- app/api/webhooks/clerks/route.ts
+++ app/api/webhooks/clerks/route.ts
@@ -50,7 +50,7 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
-  
+  const { id } = evt.data;
   const eventType = evt.type;
   const client = await clerkClient()
   
--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher(['/(.*)','/sign-in(.*)', '/sign-up(.*)', '/events/:id(.*)','/api/webhooks/clerks(.*)','/api/webhook/stripe(.*)','/api/uploadthing(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
-  if (isPublicRoute(request)) {
+  if (!isPublicRoute(request)) {
     await auth.protect()
   }
 })
