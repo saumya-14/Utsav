341eecbea2b4e9d33a633488ece181c42e6d77b5
saumya-14 <saumya140904@gmail.com>
2025-01-07 17:00:02 +0530

    form action

---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,24 @@
+"use server"
+
+import { CreateEventParams } from "@/types"
+import { connectToDatabase } from "../database"
+import User from "../database/models/user.model"
+import { revalidatePath } from "next/cache"
+import { handleError } from "../utils"
+import Event from "../database/models/event.model"
+
+export  const createEvent=async({userId,event,path}:CreateEventParams)=>{
+    try {
+        await connectToDatabase()
+    
+        const organizer = await User.findById(userId)
+        if (!organizer) throw new Error('Organizer not found')
+    
+        const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
+        revalidatePath(path)
+    
+        return JSON.parse(JSON.stringify(newEvent))
+      } catch (error) {
+        handleError(error)
+      }
+}
